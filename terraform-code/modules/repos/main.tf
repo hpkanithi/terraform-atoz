d34302dfb37841a57ca7fcf9f8911d3e9917486c
HPKanithi <167137215+hpkanithi@users.noreply.github.com>
2026-06-29 00:50:32 +0000

    fixed locals

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -1,4 +1,3 @@
-
 resource "local_file" "repos" {
   content = jsonencode(local.repos)
   filename = "${path.module}/repos.json"
@@ -9,7 +8,8 @@ module "repos" {
   for_each = var.environments
   repo_max = 9
   env      = each.key
-  repos    = jsondecode(file("repos.json"))
+  # repos    = jsondecode(file("repos.json"))
+  repos = local.repos
 }
 
 module "key" {
